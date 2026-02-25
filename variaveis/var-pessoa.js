c137dbda4005fdbc17ed05aacf91955edcf80246
profLedson <ledson.vanini@escola.pr.gov.br>
2026-02-25 13:28:53 +0000

    variaveis para pessoa

---
+++ variaveis/var-pessoa.js
@@ -0,0 +1,16 @@
+// , var, let, const
+// tipos de dados
+
+let nome = "Petter"       // string
+let sobrenome = "Parker"  // string
+const cpf = "88888888-98" // string
+const rg = "132131 ssp"   // string
+let idade = 22            // number
+let solteiro = true      // boolean
+let nota  = 5  + 3       // number
+
+console.log(nome)
+console.log(nome)
+console.log(nome)
+console.log(nome)
+console.log(nome)
