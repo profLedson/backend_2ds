9a093b0cfd41a63fc2a6b6e76e827760441e1635
profLedson <ledson.vanini@escola.pr.gov.br>
2026-06-17 11:40:14 +0000

    Finalizado tratamento de erros com Chalk

--- 2º TRIMESTRE/modulos/fs/mod-fs-path.js
+++ 2º TRIMESTRE/modulos/fs/mod-fs-path.js
@@ -1,15 +1,16 @@
 
 const fs = require('fs')
 const path = require('path')
-const chalk = require('chalk').default
+const chalk = require('chalk').default // padrão
 
 try {
     //              módulo-fs               diretório   arquivo   codificação
-    const arquivo = fs.readFileSync(path.join(__dirname, '/festa.md'), 'utf-8')
+    const arquivo = fs.readFileSync(path.join(__dirname, '/festamd'), 'utf-8')
     console.log(chalk.green('Arquivo carregado com sucesso!'))
     console.log(arquivo)
-} catch (erro) {
-    console.log(chalk.red("Deu ruim:"))
-    console.log(erro.message)
+} catch (error) {
+    console.log(chalk.bgYellowBright.red('☢️ Atenção, erro a frente'))
+    console.log(chalk.blue.bgRed.bold("🛑 Deu ruim:"))
+    console.log(error.message)
 }
 
